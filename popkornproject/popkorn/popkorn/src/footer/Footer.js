cb4406ae7e12b6dfbaaea99aa000ffcd725afdd4
agr4005 <144239265+agr4005@users.noreply.github.com>
2024-05-10 14:59:02 +0900

    담당파트 수정

--- popkornproject/popkorn/popkorn/src/footer/Footer.js
+++ popkornproject/popkorn/popkorn/src/footer/Footer.js
@@ -17,11 +17,13 @@ export default function Footer() {
             </div>
               <UseTerms />
             <dl>
-              <dt className='footer-dt'>Project Name :&nbsp;</dt><dd> PopKorn</dd>
-              <dt className='footer-dt'>Login, Join, Mypage :&nbsp; </dt><dd>Kim su bin</dd>
-              <dt className='footer-dt'>Main, Admin : &nbsp; </dt><dd>Lee Jung Hyuk </dd>
-              <dt className='footer-dt'>ProductDetail, Cart :&nbsp; </dt><dd>Lee Hye Na</dd>
-              <dt className='footer-dt'>ProductPage :&nbsp;</dt><dd>Jeon Ye won</dd>
+              <dt className='footer-dt'>프로젝트 명 :&nbsp;</dt><dd> PopKorn</dd>
+              <dt className='footer-dt'>담당파트 </dt><br/><dd>
+                1. 회원 관련 전반적인 부분(회원가입, 로그인, 마이페이지)<br/>
+                2. 관리자 페이지 - 주문목록, 배송, 이메일 <br/>
+                3. 비회원 주문 조회<br/>
+                4. Q&A 게시판
+              </dd>
             </dl>
           </div>
           <div className='footer-logo'>
